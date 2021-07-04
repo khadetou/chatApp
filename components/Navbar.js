67aa66e59bfb65c9421d1088f3081779d2eea848
khadetou <khadetou96@gmail.com>
2021-07-04 08:27:33 +0000

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,46 @@
 import Styles from '../styles/style.module.scss'
 const Navbar = () => {
-  const { navContent } = Styles
-  return <div className={navContent}>This is the navigation bar</div>
+  const {
+    button,
+    navContent,
+    container,
+    wrapper,
+    navContent__container,
+    navContent__brand,
+    navContent__brandLight,
+    navContent__links,
+    navContent__linkC,
+
+    navContent__linkT,
+  } = Styles
+  return (
+    <div className={navContent}>
+      <div className={container}>
+        <div className={`${wrapper} ${navContent__container}`}>
+          <a href="#" className={navContent__brand}>
+            Chat<span className={navContent__brandLight}>app</span>
+          </a>
+          <nav className={navContent__links}>
+            <a href="#" className={navContent__linkC}>
+              Product
+            </a>
+            <a href="#" className={navContent__linkC}>
+              Services
+            </a>
+            <a href="#" className={navContent__linkC}>
+              Contact
+            </a>
+            <a href="#" className={navContent__linkC}>
+              Log In
+            </a>
+            <a href="#" className={`${button} ${navContent__linkT}`}>
+              Try It Free
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
--- components/Section.js
+++ components/Section.js
@@ -1,4 +1,6 @@
 import Styles from '../styles/style.module.scss'
+import heroImage from '../image/Group.png'
+import Image from 'next/image'
 const Section = () => {
   const {
     section,
@@ -10,21 +12,34 @@ const Section = () => {
     button,
     section__try,
     section__free,
+    section__imageContainer,
+    section__textContainer,
   } = Styles
   return (
     <div className={section}>
       <div className={wrapper}>
         <div className={`${container} ${section__container}`}>
-          <h1 className={section__h1}>Have your best call</h1>
-          <p className={section__p}>
-            Fast, easy & unlimited conference coll services.{' '}
-          </p>
-          <a href="#" class={`${button} ${section__try}`}>
-            Try it free
-          </a>
-          <a href="#" class={`${button} ${section__free}`}>
-            Get a Demo
-          </a>
+          <div className={section__textContainer}>
+            <h1 className={section__h1}>Have your best call</h1>
+            <p className={section__p}>
+              Fast, easy & unlimited conference coll services.{' '}
+            </p>
+            <a href="#" className={`${button} ${section__try}`}>
+              Try it free
+            </a>
+            <a href="#" className={`${button} ${section__free}`}>
+              Get a Demo
+            </a>
+          </div>
+
+          <div className={section__imageContainer}>
+            <Image
+              src={heroImage}
+              width={560}
+              height={574}
+              alt="Chatting image"
+            />
+          </div>
         </div>
       </div>
     </div>
